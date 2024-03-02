04016dd1cb0d563287d6e91736e1934577f5d6c4
Bryce Mecum <petridish@gmail.com>
2024-03-02 09:11:47 -0900

    Compiling but wrong base Rust writer

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,3 +1,4 @@
+
 fn main() {
   libgromnie::test();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,106 @@
-use deku::prelude::*;
+use std::{io::{Cursor, Seek, SeekFrom, Write}, time::{SystemTime, UNIX_EPOCH}};
 
-#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
-#[deku(endian = "big")]
-struct DekuTest {
-    protocol_version_length: u8,
-    #[deku(count = "protocol_version_length")]
-    protocol_version: Vec<u8>,
+fn on_serialize<W: Write + Seek>(writer: &mut W) {
+  let protocol_version = "1804";
+
+  writer.write(&(protocol_version.len() as u16).to_le_bytes()).unwrap();
+  writer.write(protocol_version.as_bytes()).unwrap();
+  let padding = 4 - (protocol_version.len() + 2) % 4;
+  println!("Padding is {}", padding);
+  writer.seek(SeekFrom::Current(padding as i64)).unwrap();
+  let account_name = "test";
+  let password = "testing";
+
+  let mut packet_len = 0;
+  let mut user_name_pad = (account_name.len() + 2) % 4;
+  let mut password_pad = 0;
+  let login_type : u8;
+
+  if (user_name_pad > 0){
+    user_name_pad = 4 - user_name_pad;
+  }
+
+  packet_len += account_name.len() + 2 + user_name_pad;
+
+
+  if (password.len() == 0) {
+      login_type = 0x0000001;
+  } else {
+      login_type = 0x0000002;
+      password_pad = (password.len() + 5) % 4;
+
+      if (password_pad > 0) {
+        password_pad = 4 - password_pad
+      };
+
+      packet_len += password.len() + 5 + password_pad;
+  }
+
+  writer.write(&(packet_len as u8).to_le_bytes()).unwrap();
+  writer.write(&(login_type).to_le_bytes()).unwrap();
+  writer.write(&(0x0 as u8).to_le_bytes()).unwrap();
+
+  let unix_time = SystemTime::now()
+  .duration_since(UNIX_EPOCH)
+  .expect("Time went backwards")
+  .as_secs() as i32;
+
+  writer.write(&unix_time.to_le_bytes()).unwrap();
+
+  // Account Name
+  writer.write(&(account_name.len() as u16).to_le_bytes()).unwrap();
+  writer.write(account_name.as_bytes()).unwrap();
+
+  // Padding for alignment
+  let user_name_pad = 4 - (account_name.len() + 2) % 4;
+
+  if user_name_pad > 0 {
+      writer.seek(SeekFrom::Current(user_name_pad as i64)).unwrap();
+  }
+
+  // Empty string
+  writer.write(&0u32.to_le_bytes()).unwrap();
+
+  // Password
+  let maybe_password = Some(password);
+
+  match maybe_password {
+    Some(password) => {
+      let password_len = password.len() + 1;
+      writer.write(&(password_len as u32).to_le_bytes()).unwrap();
+      writer.write(&(password.len() as u8).to_le_bytes()).unwrap();
+      writer.write(password.as_bytes()).unwrap();
+
+      // Padding for alignment
+      let password_pad = 4 - (password_len as usize) % 4;
+      if password_pad > 0 {
+          writer.seek(SeekFrom::Current(password_pad as i64)).unwrap();
+      }
+    },
+    _ => {
+      writer.write(&0u32.to_le_bytes()).unwrap();
+    }
+  }
+
+  // Data versions
+  writer.write(&0x0000001Cu32.to_le_bytes()).unwrap(); // Length
+  writer.write(&0x00000016u32.to_le_bytes()).unwrap(); // Engine
+  writer.write(&0x00000000u32.to_le_bytes()).unwrap(); // Game
+
+  // Major versions
+  writer.write(&0x4C46722F34A7D7D2u64.to_le_bytes()).unwrap();
+  writer.write(&0xFD6F854F51EFB48Au64.to_le_bytes()).unwrap();
+
+  // Minor version
+  writer.write(&0x00001A01u32.to_le_bytes()).unwrap();
 }
 
 pub fn test() {
-  let data: Vec<u8> = vec![0x04, 0x00, 0x00, 0x00, 0x00];
+  let mut buffer = Cursor::new(Vec::new());
 
-  let (_rest, val) = DekuTest::from_bytes((data.as_ref(), 0)).unwrap();
+  on_serialize(&mut buffer);
+  let serialized_data = buffer.into_inner();
 
-  assert_eq!(DekuTest {
-    protocol_version_length: 0x04,
-    protocol_version: vec![0x00, 0x00, 0x00, 0x00],
-  }, val);
-
-  let data_out = val.to_bytes().unwrap();
-  assert_eq!(vec![0x04, 0x00, 0x00, 0x00, 0x00], data_out);
+  println!("len is {}", serialized_data.len());
+  println!("{:?}", serialized_data);
 }
