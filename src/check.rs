fe9dda1e45460a03dfad45bbadac53ae585a4e7e
Bryce Mecum <petridish@gmail.com>
2024-03-04 21:10:35 -0900

    Clean up check.rs

--- src/check.rs
+++ src/check.rs
@@ -1,21 +1,9 @@
 pub mod client;
 
-use std::env;
-
 use client::Client;
 
 #[tokio::main]
 async fn main() -> Result<(),()> {
-    println!("main");
-    // let args: Vec<String> = env::args().collect();
-
-    // if args.len() != 3 {
-    //     return Err((), ("wha"));
-    // }
-
-    // let bind_addr = &args[1];
-    // let target_addr = &args[2];
-
     let bind_addr = "127.0.0.1:51472";
     let target_addr = "127.0.0.1:9000";
 
@@ -25,5 +13,6 @@ async fn main() -> Result<(),()> {
         Ok(received) => println!("Success: Received {} bytes.", received),
         Err(e) => println!("Failed: recv function failed: {e:?}"),
     }
+
     Ok(())
 }
