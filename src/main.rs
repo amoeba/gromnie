540cc967e4a73654caf8eb5be8749b8e42f1bd7c
Bryce Mecum <petridish@gmail.com>
2024-03-06 21:10:19 -0900

    First working multi-client impl

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,56 @@ mod client;
 use client::Client;
 use tokio::task;
 
+async fn client_task(address: String, account_name: String, password: String) {
+    let mut client = Client::create(
+        address.to_owned(),
+        account_name.to_owned(),
+        password.to_owned(),
+    )
+    .await;
+
+    client.connect().await;
+    client.do_login().await;
+
+    let mut buf = [0u8; 1024];
+    loop {
+        // TODO: Convert use of io::Socket to tokio::Socket, then await this
+        let (size, peer) = client.socket.recv_from(&mut buf).await.unwrap();
+
+        let local_addr = client.socket.local_addr().unwrap();
+        println!(
+            "Client on port {} received data from {}: {:?}",
+            local_addr.port(),
+            peer,
+            &buf[..size]
+        );
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), ()> {
-    let mut tasks = Vec::new();
+    // TODO: Wrap this up nicer
+    let address = "localhost:9000";
+    let account_name_prefix = "test";
+    let password = "password";
 
-    let task = task::spawn(async move {
-        let target_addr = "127.0.0.1:9000";
-        let mut client = Client::create(
-            target_addr.to_owned(),
-            "test".to_owned(),
-            "testing".to_owned(),
-        )
-        .await;
+    let n = 2;
+    let mut tasks = Vec::with_capacity(2);
 
-        client.connect().await;
-        client.do_login().await;
+    for i in 0..n {
+        let mut account_name = account_name_prefix.to_owned();
+        let suffix = i.to_string();
+        account_name.push_str(suffix.as_ref());
 
-        let mut buf = [0u8; 1024];
-        loop {
-            let local_addr = client.socket.local_addr().unwrap();
-
-            // TODO: Convert use of io::Socket to tokio::Socket, then await this
-            let (size, peer) = client.socket.recv_from(&mut buf).await.unwrap();
-            println!(
-                "Client on port {} received data from {}: {:?}",
-                local_addr.port(),
-                peer,
-                &buf[..size]
-            );
-        }
-    });
-    tasks.push(task);
+        tasks.push(tokio::spawn(client_task(
+            address.to_owned(),
+            account_name.to_owned(),
+            password.to_owned(),
+        )));
+    }
 
     for task in tasks {
+        println!("about to await a task...");
         task.await.unwrap();
     }
 
