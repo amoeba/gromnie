32df8574c5df2ec09475f1ab96ac14f78bf507ed
Bryce Mecum <petridish@gmail.com>
2024-03-09 12:27:29 -0900

    Update message_test.rs

--- tests/message_test.rs
+++ tests/message_test.rs
@@ -6,8 +6,8 @@ use gromnie::messages::login_request::login_request;
 fn test_message_login_request() {
     let account_name = "mudlurk";
     let password = "mosswart";
-    let mut buffer: Cursor<Vec<u8>> = Cursor::new(Vec::new());
 
+    let mut buffer: Cursor<Vec<u8>> = Cursor::new(Vec::new());
     login_request(&mut buffer, &account_name, password);
 
     let expected = vec![0, 0, 0, 0, 0, 0, 1, 0, 147, 0, 208, 5, 0, 0, 0, 0, 64, 0, 0, 0, 4, 0, 49, 56, 48, 50, 0, 0, 52, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 62, 184, 168, 88, 28, 0, 97, 99, 115, 101, 114, 118, 101, 114, 116, 114, 97, 99, 107, 101, 114, 58, 106, 106, 57, 104, 50, 54, 104, 99, 115, 103, 103, 99, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
