6b20cd5fe24a9ff781de3533b2c1ae5e43323c38
Bryce Mecum <petridish@gmail.com>
2024-03-10 21:27:21 -0800

    Tidy up comments in LoginRequestPacket

--- src/net/packets/login_request.rs
+++ src/net/packets/login_request.rs
@@ -28,45 +28,46 @@ impl LoginRequestPacket {
     let remaining: u32 = (account_len as u32) + 24; // +24 comes from: u32 + u32 + u32 + u16 + 10
     let packet_len: u16 = 8 + (remaining as u16) + account_len - 24;
 
-    // sequence
+    // Sequence
     writer.write(&0x0u32.to_le_bytes()).unwrap();
 
-    // packetheaderflags
+    // Flags
     writer.write(&0x00010000u32.to_le_bytes()).unwrap();
 
-    // checksum
+    // Checksum
     writer.write(&0x05d00093u32.to_le_bytes()).unwrap();
 
-    // recipient
+    // RecipientId
     writer.write(&0x0u16.to_le_bytes()).unwrap();
 
-    // timesincelastpacket
+    // TimeSinceLastPacket
     writer.write(&0x0u16.to_le_bytes()).unwrap();
 
-    // size
+    // Size
     writer.write(&packet_len.to_le_bytes()).unwrap();
 
-    // iteration
+    // Iteration
     writer.write(&0x0u16.to_le_bytes()).unwrap();
 
-    // ClientVersion = packet.DataReader.ReadString16L();      // should be "1802" for end of retail client
+    // Begin LoginRequest
+    // ClientVersion
     writer.write(&0x04u16.to_le_bytes()).unwrap();
     let client_version: [u8; 6] = [0x31, 0x38, 0x30, 0x32, 0x00, 0x00];
     writer.write(&client_version).unwrap();
 
-    // uint len = packet.DataReader.ReadUInt32();                     // data length left in packet including ticket
+    // Length
     writer.write(&remaining.to_le_bytes()).unwrap();
 
-    // NetAuthType = (NetAuthType)packet.DataReader.ReadUInt32();
+    // AuthType
     writer.write(&0x01u32.to_le_bytes()).unwrap();
 
-    // var authFlags = (AuthFlags)packet.DataReader.ReadUInt32();
+    // Flags
     writer.write(&0x0u32.to_le_bytes()).unwrap();
 
-    // Timestamp = packet.DataReader.ReadUInt32();                    // sequence
+    // Sequence
     writer.write(&0x58a8b83eu32.to_le_bytes()).unwrap();
 
-    // TODO: Encapsulte string writing better, but here it is
+    // Account
     let mut token: String = self.account_name.to_owned();
     token.push_str(":");
     token.push_str(&self.password.to_owned());
@@ -74,6 +75,7 @@ impl LoginRequestPacket {
     writer.write(&(token.len() as u16).to_le_bytes()).unwrap();
     writer.write(&token.as_bytes()).unwrap();
 
+    // TODO: Not sure about the remainder but this works for now
     writer
         .write(vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0].as_ref())
         .unwrap();
