ae9080b70b2a4ed2d0fd48dcb94ec85707b21f0d
Bryce Mecum <petridish@gmail.com>
2024-03-10 21:23:41 -0800

    Refactor

--- src/net/packets/login_request.rs
+++ src/net/packets/login_request.rs
@@ -23,8 +23,10 @@ impl LoginRequestPacket {
 
 impl LoginRequestPacket {
   pub fn serialize<W: Write + Seek>(&mut self, writer: &mut W) {
-    println!("LoginRequestPacket.serialize");
-    self.packet.serialize(writer);
+    // Calculate lengths ahead of time
+    let account_len: u16 = (self.account_name.len() + self.password.len() + 1) as u16;
+    let remaining: u32 = (account_len as u32) + 24; // +24 comes from: u32 + u32 + u32 + u16 + 10
+    let packet_len: u16 = 8 + (remaining as u16) + account_len - 24;
 
     // sequence
     writer.write(&0x0u32.to_le_bytes()).unwrap();
@@ -32,10 +34,7 @@ impl LoginRequestPacket {
     // packetheaderflags
     writer.write(&0x00010000u32.to_le_bytes()).unwrap();
 
-    // checksum (packet checksum)
-    // TODO: Need to make this dynamic so we can use any login
-    // ACE has this code:
-    //  uint fragmentChecksum = Hash32.Calculate(buffer, buffer.Length) + Hash32.Calculate(Data, Data.Length);
+    // checksum
     writer.write(&0x05d00093u32.to_le_bytes()).unwrap();
 
     // recipient
@@ -44,46 +43,37 @@ impl LoginRequestPacket {
     // timesincelastpacket
     writer.write(&0x0u16.to_le_bytes()).unwrap();
 
-    // Various ahead of time length calculations
-    // TODO: This might be a little weird right now
-    let account_len: u16 = (self.account_name.len() + self.password.len() + 1) as u16;
-    let remaining: u32 = (account_len as u32) + 24; // +24 comes from: u32 + u32 + u32 + u16 + 10
-    let packet_len: u16 = 8 + (remaining as u16) + account_len - 24;
-
     // size
     writer.write(&packet_len.to_le_bytes()).unwrap();
 
     // iteration
     writer.write(&0x0u16.to_le_bytes()).unwrap();
 
-    // ClientVersion
+    // ClientVersion = packet.DataReader.ReadString16L();      // should be "1802" for end of retail client
     writer.write(&0x04u16.to_le_bytes()).unwrap();
     let client_version: [u8; 6] = [0x31, 0x38, 0x30, 0x32, 0x00, 0x00];
     writer.write(&client_version).unwrap();
 
-    // Length
+    // uint len = packet.DataReader.ReadUInt32();                     // data length left in packet including ticket
     writer.write(&remaining.to_le_bytes()).unwrap();
 
-    // AuthType
-    // TODO: This is hardcoded as Password and GlsTicket mode isn't supported
+    // NetAuthType = (NetAuthType)packet.DataReader.ReadUInt32();
     writer.write(&0x01u32.to_le_bytes()).unwrap();
 
-    // Flags
+    // var authFlags = (AuthFlags)packet.DataReader.ReadUInt32();
     writer.write(&0x0u32.to_le_bytes()).unwrap();
 
-    // Sequence
-    // TODO: I think this is supposed to be a timestamp
+    // Timestamp = packet.DataReader.ReadUInt32();                    // sequence
     writer.write(&0x58a8b83eu32.to_le_bytes()).unwrap();
 
-    // Account
-    // AccountToLoginAs (admin only)
-    // AuthType.Password | AuthType.GlsTicket
-    // writer.write(&self.account_len.to_le_bytes()).unwrap();
-    writer.write(&self.account_name.as_bytes()).unwrap();
-    writer.write(&":".as_bytes()).unwrap();
-    writer.write(&self.password.as_bytes()).unwrap();
+    // TODO: Encapsulte string writing better, but here it is
+    let mut token: String = self.account_name.to_owned();
+    token.push_str(":");
+    token.push_str(&self.password.to_owned());
 
-    // TODO: Not sure what this extra is yet
+    writer.write(&(token.len() as u16).to_le_bytes()).unwrap();
+    writer.write(&token.as_bytes()).unwrap();
+
     writer
         .write(vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0].as_ref())
         .unwrap();
