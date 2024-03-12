51dcd63ff1daf26be3f2c72808abff0b40bf21af
Bryce Mecum <petridish@gmail.com>
2024-03-11 21:40:21 -0800

    Make get_magic_number take a read cursor

--- src/crypto/magic_number.rs
+++ src/crypto/magic_number.rs
@@ -1,3 +1,5 @@
-pub fn get_magic_number(buffer: Vec<u8>, size: usize) -> u32{
+use std::io::{Read, Seek};
+
+pub fn get_magic_number<R: Read + Seek>(buffer: R, size: usize) -> u32{
   return 0;
 }
--- tests/crpyto_test.rs
+++ tests/crpyto_test.rs
@@ -1,6 +1,8 @@
+use std::io::Cursor;
+
 use gromnie::crypto::magic_number::get_magic_number;
 
 #[test]
 fn test_get_magic_number() {
-  assert_eq!(0, get_magic_number(vec![0, 0, 0, 0], 4));
+  assert_eq!(0, get_magic_number(Cursor::new(vec![0, 0, 0, 0]), 4));
 }
