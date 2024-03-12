272fc035efcebe6f9e21b08ee41e706eb6439528
Bryce Mecum <petridish@gmail.com>
2024-03-11 21:41:17 -0800

    Add real magic number test case

--- tests/crpyto_test.rs
+++ tests/crpyto_test.rs
@@ -4,5 +4,6 @@ use gromnie::crypto::magic_number::get_magic_number;
 
 #[test]
 fn test_get_magic_number() {
-  assert_eq!(0, get_magic_number(Cursor::new(vec![0, 0, 0, 0]), 4));
+  // Expected value came from debugging actestclient
+  assert_eq!(0x1020300, get_magic_number(Cursor::new(vec![1, 2, 3]), 3));
 }
