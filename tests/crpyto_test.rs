252537c911cb208d949a5ce6c4a82d1acc75009b
Bryce Mecum <petridish@gmail.com>
2024-03-14 18:55:57 -0800

    Passing test

--- tests/crpyto_test.rs
+++ tests/crpyto_test.rs
@@ -6,5 +6,5 @@ use gromnie::crypto::magic_number::get_magic_number;
 fn test_get_magic_number() {
   // Expected value came from debugging actestclient
   let input = vec![1, 2, 3];
-  assert_eq!(0x1020300, get_magic_number(&input, 3, true));
+  assert_eq!(0x1020300, get_magic_number(&input, 3, false));
 }
