e8e215c39da53e65f0b162f0088b3d1775d95ef2
Bryce Mecum <petridish@gmail.com>
2024-03-14 18:55:04 -0800

    Working but failing get_magic_number

--- src/crypto/magic_number.rs
+++ src/crypto/magic_number.rs
@@ -1,5 +1,27 @@
-use std::io::{Read, Seek};
+pub fn get_magic_number(buffer: &Vec<u8>, size: usize, include_size: bool) -> i32{
+  let mut magic : i32 = 0;
 
-pub fn get_magic_number<R: Read + Seek>(buffer: R, size: usize) -> u32{
-  return 0;
+  if include_size {
+    magic += (size as i32) << 16;
+  }
+
+  // i is used in both for loops
+  let mut i : u32 = 0;
+
+  for _ in 0..(size / 4) {
+    magic += buffer[i as usize] as i32;
+    i += 1;
+  }
+
+  let mut shift = 3;
+
+  i = i * 4;
+
+  for _ in i..(size as u32) {
+    magic += (buffer[i as usize] as i32) << (shift * 8);
+    shift -= 1;
+    i += 1;
+  }
+
+  magic
 }
--- tests/crpyto_test.rs
+++ tests/crpyto_test.rs
@@ -5,5 +5,6 @@ use gromnie::crypto::magic_number::get_magic_number;
 #[test]
 fn test_get_magic_number() {
   // Expected value came from debugging actestclient
-  assert_eq!(0x1020300, get_magic_number(Cursor::new(vec![1, 2, 3]), 3));
+  let input = vec![1, 2, 3];
+  assert_eq!(0x1020300, get_magic_number(&input, 3, true));
 }
