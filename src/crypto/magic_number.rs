d91e66b348558b2c68167d7a41d6b9615d64859c
Bryce Mecum <petridish@gmail.com>
2024-03-24 20:42:03 -0800

    Update magic_number.rs

--- src/crypto/magic_number.rs
+++ src/crypto/magic_number.rs
@@ -7,15 +7,11 @@ pub fn get_magic_number(buffer: &[u8], size: usize, include_size: bool) -> u32{
     magic += (size as u32) << 16;
   }
 
-  // i is used in both for loops
   let mut i : i32 = 0;
 
   for _ in 0..(size / 4) {
     let start = (i * 4) as usize;
-    println!("current {}; next {}", magic, LittleEndian::read_u32(&buffer[start..(start+4)]));
-
     magic = magic.wrapping_add(LittleEndian::read_u32(&buffer[start..(start+4)]));
-
     i += 1;
   }
 
@@ -24,8 +20,6 @@ pub fn get_magic_number(buffer: &[u8], size: usize, include_size: bool) -> u32{
   i = i * 4;
 
   for _ in i..(size as i32) {
-    println!("current {}; next {}", magic, (buffer[i as usize] as u32) << (shift * 8));
-
     magic = magic.wrapping_add((buffer[i as usize] as u32) << (shift * 8));
     shift -= 1;
     i += 1;
