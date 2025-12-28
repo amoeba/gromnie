f9eaca16a39a010976b1471a63edcf22c9d7f9e7
Bryce Mecum <petridish@gmail.com>
2025-12-28 12:53:00 -0800

    repo cleanup

--- test_messages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~/src/amoeba/gromnie
-rm -f /tmp/gromnie-test.log
-RUST_LOG=info ./target/debug/gromnie 2>&1 | tee /tmp/gromnie-test.log &
-PID=$!
-sleep 15
-kill $PID
-echo "===== ALL MESSAGES RECEIVED ====="
-grep "Parsed as S2CMessage\|Unhandled S2CMessage\|Unknown message\|OrderedGameEvent" /tmp/gromnie-test.log
