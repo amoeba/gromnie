f4d70ef907b1da828070b63fad45e53f3c29b32b
Bryce Mecum <petridish@gmail.com>
2025-12-28 12:53:33 -0800

    set up cargo dist

--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -5,6 +5,9 @@ mod event_consumer;
 pub use character_gen::CharacterBuilder;
 pub use client_runner::{run_client, run_client_with_action_channel, ClientConfig};
 pub use event_consumer::{
-    handle_character_list, handle_character_list_with_name, DiscordConsumer, EventConsumer,
-    LoggingConsumer, TuiConsumer, UptimeData,
+    handle_character_list, handle_character_list_with_name, EventConsumer, LoggingConsumer,
+    TuiConsumer,
 };
+
+#[cfg(feature = "discord")]
+pub use event_consumer::{DiscordConsumer, UptimeData};
