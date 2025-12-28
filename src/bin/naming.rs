d2b09dc57723066b71fe45a6a4c801127a67c64a
Bryce Mecum <petridish@gmail.com>
2025-12-28 11:21:37 -0800

    cargo fmt

--- src/bin/naming.rs
+++ src/bin/naming.rs
@@ -2,16 +2,16 @@ use gromnie::load_tester::ClientNaming;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    
+
     if args.len() < 2 {
         println!("Usage: {} <client_id>", args[0]);
         println!("Generates account and character names for load testing");
         std::process::exit(1);
     }
-    
+
     let client_id: u32 = args[1].parse().expect("Invalid client ID");
     let naming = ClientNaming::new(client_id);
-    
+
     println!("Client ID: {}", client_id);
     println!("Account: {}", naming.account_name());
     println!("Password: {}", naming.password());
